331492a1c1861ecab8725cb5b9bcaf94785290e0
prtsqof <prtsqof@gmail.com>
2019-02-03 12:46:49 +0300

    Less1

--- python.py
+++ python.py
@@ -2,3 +2,25 @@ print("It`s possible to be winner for long live in Russia")
 print("hellow")
 name = input("Your name: ")
 print(name, ", welcome!")
+
+answer = input("Do u wont get stat? (Y/N)")
+
+if answer == "Y":
+	print("Good job MAN!")
+	kind = input("What do u wont to do? 1 - Learning more/2 - Touch same boobs?/3 - get out?")
+
+	if kind == "1":
+		print("Good Job MAN, U can fuck the world!")
+	elif kind == "2":
+		print("hellow")
+	elif kind == "3":
+		print("good")
+	else:
+		pass
+	
+
+
+elif answer == "N":
+	print("fuck")
+else:
+	print(name, ",fuck u")
